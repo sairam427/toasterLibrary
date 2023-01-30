864fd454476ec2f151c2d396771947ca9926b803
sairam427 <ramreddy.rachamalla@creditvidya.com>
2023-01-30 11:33:44 +0530

    Initial commits

---
+++ toaster/src/main/java/com/example/toaster/ToastMessage.kt
@@ -0,0 +1,10 @@
+package com.example.toaster
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastMessage {
+    fun showToast(c: Context, message: String) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
